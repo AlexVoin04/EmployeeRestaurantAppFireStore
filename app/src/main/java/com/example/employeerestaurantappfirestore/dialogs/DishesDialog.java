f44916a467f24a20b146e4f369ba3f15a69157c6
Alex <alexiv.04@mail.ru>
2024-02-29 22:33:27 +0700

    Adding search and filter in DishesDialog

--- app/src/main/java/com/example/employeerestaurantappfirestore/dialogs/DishesDialog.java
+++ app/src/main/java/com/example/employeerestaurantappfirestore/dialogs/DishesDialog.java
@@ -6,13 +6,17 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +25,7 @@ import com.example.employeerestaurantappfirestore.R;
 import com.example.employeerestaurantappfirestore.adapters.DishAdapter;
 import com.example.employeerestaurantappfirestore.managers.MenuManager;
 import com.example.employeerestaurantappfirestore.model.ModelDishes;
+import com.example.employeerestaurantappfirestore.model.ModelTableList;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,12 +38,17 @@ public class DishesDialog extends Dialog {
     private final Context context;
     private FirebaseFirestore fireStore;
     private List<ModelDishes> dishes;
+    private List<ModelDishes> addedDishes;
     private RecyclerView rv_dishes;
     private NestedScrollView nsv_dishes;
     private EditText et_comment;
     private LinearLayout ll_btn_cancellation, ll_btn_added_dishes;
     private DishAdapter dishAdapter;
     private Spinner spin_dish_type;
+    private SearchView sv_dishes;
+    private int filterPos;
+    private String queryText;
+    private RelativeLayout rl_dishes_not_found;
 
     public DishesDialog(@NonNull Context context) {
         super(context);
@@ -72,6 +82,8 @@ public class DishesDialog extends Dialog {
         ll_btn_cancellation = findViewById(R.id.ll_btn_cancellation);
         ll_btn_added_dishes = findViewById(R.id.ll_btn_added_dishes);
         spin_dish_type = findViewById(R.id.spin_dish_type);
+        sv_dishes = findViewById(R.id.sv_dishes);
+        rl_dishes_not_found = findViewById(R.id.rl_dishes_not_found);
     }
 
     private void initAdapter() {
@@ -91,13 +103,65 @@ public class DishesDialog extends Dialog {
 
     private void initListeners(){
         ll_btn_cancellation.setOnClickListener(view -> dismiss());
+        spin_dish_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                filterPos = pos;
+                getDishes();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        sv_dishes.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                queryText = query;
+                getDishes();
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String query) {
+                queryText = query;
+                getDishes();
+                return false;
+            }
+        });
+    }
+
+    private void setStatusVisible(int status1, int status2){
+        rl_dishes_not_found.setVisibility(status1);
+        nsv_dishes.setVisibility(status2);
+    }
+
+    private List<ModelDishes> filterDishesByType(int type) {
+        List<ModelDishes> filteredList = new ArrayList<>();
+        for (ModelDishes dish : dishes) {
+            Log.d("test", dish.getAbbreviatedName() + " " + dish.getIdTypeOfTheDishes().getId() + " " + type);
+            if (dish.getIdTypeOfTheDishes().getId().equals(String.valueOf(type))) {
+                filteredList.add(dish);
+            }
+        }
+        return filteredList;
+    }
+
+    private List<ModelDishes> searchDishes() {
+        List<ModelDishes> foundDishes = new ArrayList<>();
+        for (ModelDishes dish : dishes){
+            if (dish.getName().toLowerCase().contains(queryText.toLowerCase())){
+                foundDishes.add(dish);
+            }
+        }
+        return foundDishes;
     }
 
     private void initData(){
         dishes = new ArrayList<>();
         fireStore = FirebaseFirestore.getInstance();
-
+        queryText = "";
     }
 
     private void getDishesMenu(){
@@ -119,6 +183,7 @@ public class DishesDialog extends Dialog {
     @SuppressLint("NotifyDataSetChanged")
     private void getDishes(){
         int scrollY = rv_dishes.getScrollY();
+        setStatusVisible(View.GONE, View.VISIBLE);
         CollectionReference dishesCollection = fireStore.collection("Dishes");
         dishesCollection.addSnapshotListener((dishesSnapshots, error) -> {
             if (error != null) {
@@ -138,6 +203,20 @@ public class DishesDialog extends Dialog {
                     dish.setId(document.getId());
                     dishes.add(dish);
                 }
+            }
+
+            if(filterPos!=0){
+                dishes = filterDishesByType(filterPos);
+                dishAdapter.notifyDataSetChanged();
+                initAdapter();
+            }
+
+            if (!Objects.equals(queryText, "") && queryText.length()>1) {
+                dishes = searchDishes();
+            }
+
+            if (dishes.size()==0){
+                setStatusVisible(View.VISIBLE, View.GONE);
             }
 
             initAdapter();
